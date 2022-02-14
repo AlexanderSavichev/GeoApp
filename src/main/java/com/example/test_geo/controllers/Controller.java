067e85eb8efc6edd0b75d9a9caa5bd639cebd274
Aleksandr <alexander.savichev@gmail.com>
2022-02-14 18:59:01 +0300

    Исправлен UriComponentsBuilder Изменен возврат в строку, а не JSON объект

--- src/main/java/com/example/test_geo/controllers/Controller.java
+++ src/main/java/com/example/test_geo/controllers/Controller.java
@@ -1,18 +1,13 @@
 package com.example.test_geo.controllers;
-
-import com.example.test_geo.models.result;
 import com.example.test_geo.service.method;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
-
 import java.net.URISyntaxException;
-import java.util.List;
 
 @RestController
 @Component
@@ -20,9 +15,21 @@ public class Controller {
     @Autowired
     method newMethod;
     @GetMapping("/param")
-    public List<result> getLocation (@RequestParam(value = "location") String location) throws URISyntaxException {
+    //public List<result> getLocation (@RequestParam(value = "location") String location) throws URISyntaxException {
+    public String getLocation (@RequestParam(value = "location") String location) throws URISyntaxException {
         UriComponents uriComponents = UriComponentsBuilder.newInstance()
-                .scheme("https").host("api.geoapify.com").path("/v1/geocode/search?text=").pathSegment(location, "&format=json&apiKey=a901afdd553443ea86f238b6c6326598").build();
-        return newMethod.getLocation(uriComponents.toUriString());
+               .scheme("https")
+               .host("api.geoapify.com")
+                //.path("/v1/geocode/search")
+                //.queryParam("text",location)
+               // .pathSegment("&format=json&apiKey=c1f45f7bcb654b4698fb833af17c4d67").encode()
+                //.queryParam("/v1/geocode/search?text=",location,  "&format=json&apiKey=c1f45f7bcb654b4698fb833af17c4d67").encode()
+                .path("/v1/geocode/search?text=")
+                .pathSegment(location, "&format=json&apiKey=c1f45f7bcb654b4698fb833af17c4d67").encode()
+                .build();
+        String newUri = uriComponents.toUriString();
+        String newUri1 = newUri.replace("%3F","?");
+        System.out.println(newUri1);
+        return newMethod.getLocation(newUri1);
     }
 }
--- src/main/java/com/example/test_geo/service/method.java
+++ src/main/java/com/example/test_geo/service/method.java
@@ -1,19 +1,21 @@
 package com.example.test_geo.service;
-
-import com.example.test_geo.models.GeoResult;
-import com.example.test_geo.models.result;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.List;
 
 @Component("newMethod")
 public class method {
     private RestTemplate restTemplate = new RestTemplate();
-    public List<result> getLocation (String newLocation) throws URISyntaxException {
-        GeoResult response = restTemplate.getForObject(new URI(newLocation), GeoResult.class);
-        return response.results;
+    //public List<result> getLocation (String newLocation) throws URISyntaxException {
+    public String getLocation (String newLocation) throws URISyntaxException {
+        //GeoResult response = restTemplate.getForObject(new URI(newLocation), GeoResult.class);
+        //JSONObject response = new JSONObject();
+        String response = restTemplate.getForObject(new URI(newLocation), String.class);
+        //String newResponse = response.toString();
+        return response;
+        //System.out.println(response);
+       // return response.results;
+
     }
 }
