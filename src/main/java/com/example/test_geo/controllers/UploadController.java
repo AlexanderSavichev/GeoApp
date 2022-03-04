2446c94cefd595f4affb3c068d3beabd58c92c5e
Aleksandr <alexander.savichev@gmail.com>
2022-03-04 22:55:26 +0300

    Черновик отправки результата пользователю3

--- src/main/java/com/example/test_geo/controllers/UploadController.java
+++ src/main/java/com/example/test_geo/controllers/UploadController.java
@@ -1,15 +1,22 @@
 package com.example.test_geo.controllers;
 import com.example.test_geo.service.Reader;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -19,8 +26,10 @@ import java.nio.file.StandardCopyOption;
 public class UploadController {
     @Autowired
     Reader NewReader;
-    @PostMapping("/upload")
-    public String uploadFile(@RequestParam("file") MultipartFile file, RedirectAttributes attributes) {
+   @PostMapping("/upload")
+    //@GetMapping("/geocodeFile.html")
+    //public @ResponseBody
+    String uploadFile(@RequestParam("file") MultipartFile file, RedirectAttributes attributes, HttpServletResponse response) {
         if (file.isEmpty()) {
             attributes.addFlashAttribute("message", "Выберите файл для загрузки");
             return "redirect:/";
@@ -32,7 +41,14 @@ public class UploadController {
             Files.copy(file.getInputStream(), tempFile, StandardCopyOption.REPLACE_EXISTING);
             tempFile.toFile().deleteOnExit();
             tempDirectory.toFile().deleteOnExit();
-            NewReader.ReadLines(tempFile);
+
+            byte[] data = NewReader.ReadLines(tempFile);
+            InputStream targetStream = new ByteArrayInputStream(data);
+            response.setContentType("text/utf-8");
+            for (int x = 0; x < data.length; x++) {
+                System.out.println((char)data[x]);
+                IOUtils.copy(targetStream, response.getOutputStream());
+            }
 
         } catch (IOException | URISyntaxException e) {
             e.printStackTrace();
--- src/main/java/com/example/test_geo/service/Reader.java
+++ src/main/java/com/example/test_geo/service/Reader.java
@@ -2,18 +2,9 @@ package com.example.test_geo.service;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.ServletInputStream;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.*;
-import java.net.HttpURLConnection;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.file.Path;
 
 @Component("NewReader")
@@ -21,11 +12,14 @@ public class Reader extends HttpServlet {
     @Autowired
     Method FileMethod;
 
-    public Byte[] ReadLines(Path FilePath) throws IOException, URISyntaxException {
+    public byte[] ReadLines(Path FilePath) throws IOException, URISyntaxException {
         BufferedReader reader = new BufferedReader(new FileReader(String.valueOf(FilePath)));
         StringBuilder content = new StringBuilder();
         String line;
         ByteArrayOutputStream ByteResponse = new ByteArrayOutputStream();
+        FileWriter writer = new FileWriter(String.valueOf(FilePath));
+        BufferedWriter buffer = new BufferedWriter(writer);
+
         while ((line = reader.readLine()) != null) {
             content.append(line);
             content.append(System.lineSeparator());
@@ -33,40 +27,16 @@ public class Reader extends HttpServlet {
             LonLocLocator FileLocator = new LonLocLocator();
             try {
                 String LonLocResponse = "\"lon\"" + FileLocator.LonLocString(FileResponse, "\"lon\"", "\"formatted\"");
+                buffer.write(LonLocResponse);
                 ByteResponse.write(LonLocResponse.getBytes());
-                ByteResponse.flush();
-                ByteResponse.close();
-               // response.setContentType("text/utf-8");
-               // ServletOutputStream out = response.getOutputStream();
-                //byte b [] = ByteResponse.toByteArray();
-                //for(int x = 0; x < b.length; x++) {
-                   //ByteResponse.writeTo(out);
-                   // out.flush();
-                    //IOUtils.copy((char) b[x], response.getOutputStream());
-                //System.out.print((char)b[x]  + "   ");
-                     //   }
-
-                //FileWriter fw = new FileWriter(String.valueOf(FilePath), true);
-                //BufferedWriter bw = new BufferedWriter(fw);
-                // bw.newLine();
-                // bw.write(LonLocResponse);
-                // bw.newLine();
-                // bw.close();
             } catch (StringIndexOutOfBoundsException e) {
                 return null;
             }
+            ByteResponse.flush();
+            ByteResponse.close();
+            buffer.close();
         }
-        return null;
+
+        return ByteResponse.toByteArray();
     }
-    @GetMapping("/geocodeFile")
-    public @ResponseBody
-    void getReviewedFile(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        ServletInputStream sis = request.getInputStream();
-        byte[] data = sis.readAllBytes();
-       response.setContentType("text/utf-8");
-       for (int x = 0; x < data.length; x++) {
-           System.out.println((char)data[x]);
-           IOUtils.copy(sis, response.getOutputStream());
-       }
-   }
 }
