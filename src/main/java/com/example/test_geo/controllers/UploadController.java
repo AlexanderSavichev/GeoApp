acdaf055993d9708721f4d5eac6b056cd2a8c265
Aleksandr <alexander.savichev@gmail.com>
2022-03-12 20:36:28 +0300

    Черновик отправки результата пользователю4

--- src/main/java/com/example/test_geo/controllers/UploadController.java
+++ src/main/java/com/example/test_geo/controllers/UploadController.java
@@ -7,27 +7,18 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import javax.servlet.ServletInputStream;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 
 @Controller
 public class UploadController {
     @Autowired
     Reader NewReader;
-   @PostMapping("/upload")
-    //@GetMapping("/geocodeFile.html")
+    @PostMapping("/upload")
+    @GetMapping("/geocodeFile.html")
     //public @ResponseBody
     String uploadFile(@RequestParam("file") MultipartFile file, RedirectAttributes attributes, HttpServletResponse response) {
         if (file.isEmpty()) {
@@ -36,18 +27,12 @@ public class UploadController {
         }
         String fileName = StringUtils.cleanPath(file.getOriginalFilename());
         try {
-            Path tempDirectory = Files.createTempDirectory(null);
-            Path tempFile = Files.createTempFile(tempDirectory, null, ".txt");
-            Files.copy(file.getInputStream(), tempFile, StandardCopyOption.REPLACE_EXISTING);
-            tempFile.toFile().deleteOnExit();
-            tempDirectory.toFile().deleteOnExit();
-
-            byte[] data = NewReader.ReadLines(tempFile);
+            byte[] data = NewReader.ReadLines(file.getInputStream());
             InputStream targetStream = new ByteArrayInputStream(data);
             response.setContentType("text/utf-8");
+            IOUtils.copy(targetStream, response.getOutputStream());
             for (int x = 0; x < data.length; x++) {
-                System.out.println((char)data[x]);
-                IOUtils.copy(targetStream, response.getOutputStream());
+                System.out.print((char)data[x] + " ");
             }
 
         } catch (IOException | URISyntaxException e) {
--- src/main/java/com/example/test_geo/service/Reader.java
+++ src/main/java/com/example/test_geo/service/Reader.java
@@ -1,24 +1,21 @@
 package com.example.test_geo.service;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServlet;
 import java.io.*;
 import java.net.URISyntaxException;
-import java.nio.file.Path;
 
 @Component("NewReader")
 public class Reader extends HttpServlet {
     @Autowired
     Method FileMethod;
 
-    public byte[] ReadLines(Path FilePath) throws IOException, URISyntaxException {
-        BufferedReader reader = new BufferedReader(new FileReader(String.valueOf(FilePath)));
+    public byte[] ReadLines(InputStream FilePath) throws IOException, URISyntaxException {
+        InputStreamReader isr = new InputStreamReader(FilePath);
+        BufferedReader reader = new BufferedReader(isr);
         StringBuilder content = new StringBuilder();
         String line;
         ByteArrayOutputStream ByteResponse = new ByteArrayOutputStream();
-        FileWriter writer = new FileWriter(String.valueOf(FilePath));
-        BufferedWriter buffer = new BufferedWriter(writer);
 
         while ((line = reader.readLine()) != null) {
             content.append(line);
@@ -27,16 +24,14 @@ public class Reader extends HttpServlet {
             LonLocLocator FileLocator = new LonLocLocator();
             try {
                 String LonLocResponse = "\"lon\"" + FileLocator.LonLocString(FileResponse, "\"lon\"", "\"formatted\"");
-                buffer.write(LonLocResponse);
                 ByteResponse.write(LonLocResponse.getBytes());
             } catch (StringIndexOutOfBoundsException e) {
                 return null;
             }
             ByteResponse.flush();
             ByteResponse.close();
-            buffer.close();
-        }
 
+        }
         return ByteResponse.toByteArray();
     }
 }
